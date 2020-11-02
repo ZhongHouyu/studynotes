4f725a8b4c7e9bc1745b52cd299fed139239f1e3
ZhongHouyu <993837013@qq.com>
2020-11-02 13:36:47 +0800

    improve problem2.cpp

--- problem/problem2.cpp
+++ problem/problem2.cpp
@@ -1,31 +1,17 @@
 #include "ListNode.h"
 ListNode* ListNode::addTwoNumbers(ListNode* l1, ListNode* l2)
 {
-    ListNode* r = new ListNode();
-    ListNode* ans = new ListNode(0, r);
-    int flag = 0;
-    while (l1 != nullptr || l2 != nullptr) {
-        l1->val = l1 == nullptr ? 0 : l1->val;
-        l2->val = l2 == nullptr ? 0 : l2->val;
-        ListNode* number = new ListNode();
-        number->val = (l1->val + l2->val + flag) % 10;
-        flag = (l1->val + l2->val + flag - 10) >= 0 ? 1 : 0;
-        if (l1!= nullptr) {
-            l1 = l1->next;
-        }
-        if (l2 != nullptr) {
-            l2 = l2->next;
-        }
-        r->next = number;
-        r = number;
-    }
-    if (flag != 0) {
-        ListNode* number = new ListNode(1);
-        r->next = number;
-        r = number;
-    }
-
-    return ans->next->next;
+    return dfs(l1, l2, 0);
 
 }
+ListNode* dfs(ListNode* l1, ListNode* l2, int i) {
+    if (l1 == nullptr && l2 == nullptr && i == 0) {
+        return nullptr;
+    }
+    int sum = (l1 == nullptr ? 0 : l1->val) + (l2 == nullptr ? 0 : l2->val) + i;
+    ListNode* number = new ListNode(sum % 10);
+    number->next = dfs(l1 == nullptr ? nullptr : l1->next, l2 == nullptr ? nullptr : l2->next, sum / 10);
+    return number;
+}
+
 
